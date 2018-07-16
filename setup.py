c90c2ab0a25cb740d221980f4be4530ce61548d3
Francois Blackburn <fblackburn@wazo.community>
2018-07-16 13:47:58 -0400

    fix styling issues

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-
 from setuptools import setup
 from setuptools import find_packages
 
--- wazo_accessd_client/client.py
+++ wazo_accessd_client/client.py
@@ -1,7 +1,6 @@
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-
 from xivo_lib_rest_client.client import BaseClient
 
 
@@ -9,13 +8,5 @@ class AccessdClient(BaseClient):
 
     namespace = 'wazo_accessd_client.commands'
 
-    def __init__(self,
-                 host,
-                 port=9486,
-                 version='1.0',
-                 **kwargs):
-        super(AccessdClient, self).__init__(
-              host=host,
-              port=port,
-              version=version,
-              **kwargs)
+    def __init__(self, host, port=9486, version='1.0', **kwargs):
+        super(AccessdClient, self).__init__(host=host, port=port, version=version, **kwargs)
