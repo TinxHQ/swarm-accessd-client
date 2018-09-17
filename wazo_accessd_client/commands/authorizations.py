e33a307218061620980f0c5fee85d3a6982cd150
Anthony Delaby <pro@anthonydelaby.me>
2018-09-17 14:30:29 +0200

    authorizations: make possible to issue a token

--- wazo_accessd_client/commands/authorizations.py
+++ wazo_accessd_client/commands/authorizations.py
@@ -13,3 +13,10 @@ class AuthorizationsCommand(BaseCommand):
         r = self.session.post(self.base_url, json=authorization, headers=headers)
         self.raise_from_response(r)
         return r.json()
+
+    def issue_token(self, authorizations_uuids, **kwargs):
+        url = '{base}/token'.format(base=self.base_url)
+        headers = self._get_headers(write=True, **kwargs)
+        r = self.session.post(url, json=authorizations_uuids, headers=headers)
+        self.raise_from_response(r)
+        return r.json()
