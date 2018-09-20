ed9d98123abb7a2ed0e25e3b3c4432df12dc7e52
Anthony Delaby <pro@anthonydelaby.me>
2018-09-20 17:53:17 +0200

    authorizations: make possible to issue a token for a subscription authorizations

--- wazo_accessd_client/commands/authorizations.py
+++ wazo_accessd_client/commands/authorizations.py
@@ -52,9 +52,18 @@ class AuthorizationsCommand(BaseCommand):
         self.raise_from_response(r)
         return r.json()
 
-    def issue_token(self, authorizations_uuids, **kwargs):
-        url = '{base}/token'.format(base=self.base_url)
+    def issue_token(self, authorizations_uuids={}, subscription_uuid=None, **kwargs):
+        if subscription_uuid:
+            url = '{base}/subscriptions/{subscription_uuid}/{resource}/token'.format(
+                base=self._client.url(),
+                subscription_uuid=subscription_uuid,
+                resource=self.resource,
+            )
+        else:
+            url = '{base}/token'.format(base=self.base_url)
         headers = self._get_headers(write=True, **kwargs)
         r = self.session.post(url, json=authorizations_uuids, headers=headers)
         self.raise_from_response(r)
+        if r.status_code == 204:
+            return
         return r.json()
