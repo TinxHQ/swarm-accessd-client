c02011136c5e4e1b9c13f1d26a57441d5d6b2ed5
Anthony Delaby <pro@anthonydelaby.me>
2018-09-27 17:25:41 +0200

    subscriptions: make possible to delete a subscription

--- wazo_accessd_client/commands/subscriptions.py
+++ wazo_accessd_client/commands/subscriptions.py
@@ -8,6 +8,9 @@ class SubscriptionsCommand(BaseCommand):
 
     resource = 'subscriptions'
 
+    def _get_detail_url(self, subscription_uuid):
+        return '{base}/{uuid}'.format(base=self.base_url, uuid=subscription_uuid)
+
     def list(self, **params):
         headers = self._get_headers(**params)
         r = self.session.get(self.base_url, headers=headers, params=params)
@@ -15,11 +18,17 @@ class SubscriptionsCommand(BaseCommand):
         return r.json()
 
     def get(self, subscription_uuid, tenant_uuid=None, **params):
-        url = '{base}/{uuid}'.format(base=self.base_url, uuid=subscription_uuid, **params)
+        url = self._get_detail_url(subscription_uuid)
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
+
+    def delete(self, subscription_uuid, tenant_uuid=None):
+        url = self._get_detail_url(subscription_uuid)
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
+        r = self.session.delete(url, headers=headers)
+        self.raise_from_response(r)
 
     def create(self, subscription, **kwargs):
         return self._post(subscription, **kwargs)
