34bc5ae60b6d6a79ba0158d96c17423e52f55821
Francois Blackburn <fblackburn@wazo.community>
2018-09-28 09:30:20 -0400

    remove indirection layer

--- wazo_accessd_client/commands/subscriptions.py
+++ wazo_accessd_client/commands/subscriptions.py
@@ -8,9 +8,6 @@ class SubscriptionsCommand(BaseCommand):
 
     resource = 'subscriptions'
 
-    def _get_detail_url(self, subscription_uuid):
-        return '{base}/{uuid}'.format(base=self.base_url, uuid=subscription_uuid)
-
     def list(self, **params):
         headers = self._get_headers(**params)
         r = self.session.get(self.base_url, headers=headers, params=params)
@@ -18,14 +15,14 @@ class SubscriptionsCommand(BaseCommand):
         return r.json()
 
     def get(self, subscription_uuid, tenant_uuid=None, **params):
-        url = self._get_detail_url(subscription_uuid)
+        url = '{base}/{uuid}'.format(base=self.base_url, uuid=subscription_uuid)
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def delete(self, subscription_uuid, tenant_uuid=None):
-        url = self._get_detail_url(subscription_uuid)
+        url = '{base}/{uuid}'.format(base=self.base_url, uuid=subscription_uuid)
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         r = self.session.delete(url, headers=headers)
         self.raise_from_response(r)
