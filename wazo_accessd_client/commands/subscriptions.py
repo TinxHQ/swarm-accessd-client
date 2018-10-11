b3e4eb856b9ca923ecf0ea61d415e5d397abf59d
Francois Blackburn <fblackburn@wazo.community>
2018-10-11 13:53:01 -0400

    remove unused method

--- wazo_accessd_client/commands/subscriptions.py
+++ wazo_accessd_client/commands/subscriptions.py
@@ -14,6 +14,12 @@ class SubscriptionsCommand(BaseCommand):
         self.raise_from_response(r)
         return r.json()
 
+    def create(self, subscription, **kwargs):
+        headers = self._get_headers(write=True, **kwargs)
+        r = self.session.post(self.base_url, json=subscription, headers=headers)
+        self.raise_from_response(r)
+        return r.json()
+
     def get(self, subscription_uuid, tenant_uuid=None):
         url = '{base}/{uuid}'.format(base=self.base_url, uuid=subscription_uuid)
         headers = self._get_headers(tenant_uuid=tenant_uuid)
@@ -33,15 +39,3 @@ class SubscriptionsCommand(BaseCommand):
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         r = self.session.delete(url, headers=headers)
         self.raise_from_response(r)
-
-    def create(self, subscription, **kwargs):
-        return self._post(subscription, **kwargs)
-
-    def amend(self, subscription, **kwargs):
-        return self._post(subscription, **kwargs)
-
-    def _post(self, subscription, **kwargs):
-        headers = self._get_headers(write=True, **kwargs)
-        r = self.session.post(self.base_url, json=subscription, headers=headers)
-        self.raise_from_response(r)
-        return r.json()
