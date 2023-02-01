7b906d414cca2687692248aff29ddadabe18b735
Francois Blackburn <fblackburn@wazo.community>
2023-02-01 08:24:22 -0500

    fix new black rules

--- swarm_accessd_client/client.py
+++ swarm_accessd_client/client.py
@@ -1,11 +1,10 @@
-# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.client import BaseClient
 
 
 class AccessdClient(BaseClient):
-
     namespace = 'swarm_accessd_client.commands'
 
     def __init__(self, host, port=443, prefix='/api/accessd', version='1.0', **kwargs):
--- swarm_accessd_client/commands/config.py
+++ swarm_accessd_client/commands/config.py
@@ -1,11 +1,10 @@
-# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from swarm_accessd_client.command import AccessdCommand
 
 
 class ConfigCommand(AccessdCommand):
-
     resource = 'config'
 
     def get(self):
